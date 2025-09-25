356bddff19e7b7bad7d6df972a17b552592fc0e2
Ximena-EI <u202214784@upc.edu.pe>
2025-09-25 12:37:14 -0500

    update: update routes for index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import ControlPanelPageComponent from "../public/pages/control-panel/components/control-panel-page.component.vue";
 
 const routes = [
     { path: '/', redirect: '/home'},
+    { path: '/control-panel', component: ControlPanelPageComponent},
 
 ];
 
