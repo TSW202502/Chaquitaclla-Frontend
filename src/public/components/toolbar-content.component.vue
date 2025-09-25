fb3e2e85c2524144da97efeac468e9dac827cfc5
RichiAbV <u201714765@upc.edu.pe>
2025-09-25 00:21:49 -0500

    chore: fixed toolbar component

--- src/public/components/toolbar-content.component.vue
+++ src/public/components/toolbar-content.component.vue
@@ -1,121 +1,99 @@
 <script>
+import LanguageSwitcher from "./language-switcher.component.vue";
 
 export default {
   name: "toolbar-content",
+  components: {LanguageSwitcher},
+
   data() {
     return {
-      items: [
-        { label: 'Statistical Reports', to: '/sowing-statistics-reports' },
-        { label: 'Crop Registration', to: '/crop-list-and-registration' },
-        { label: 'Consultation forum', to: '/consultation-forum' },
-        { label: 'Crop History', to: '/crop-history' }
-      ],
-      recommendedCrop: null
-    }
+      isLoggedIn: false,
+    };
   },
-  created() {
-
-  }
+  methods: {
+    login() { this.isLoggedIn = true; },
+    logout() { this.isLoggedIn = false; },
+  },
 }
 </script>
 
 <template>
-  <div class="main-container">
-    <div class="left-container">
-      <pv-card class="justify-content-center mt-2">
-        <template #content>
-          <i class="pi pi-chart-bar icon-large"></i>
-        </template>
-        <template #footer>
-          <router-link :to="items[0].to"  rel="noopener">
-            <pv-button class="button-green" :label="items[0].label" />
-          </router-link>
-        </template>
-      </pv-card>
+  <pv-toolbar class="fixed-toolbar pv-toolbar" style=" padding: 1rem 1rem 1rem 1.5rem">
+    <template #start>
+      <span class="toolbar-section">
+        <img src="https://raw.githubusercontent.com/upc-pre-202401-si730-ws53-Error-404/TB1_AplicacionesWeb/main/resources/ChaquitacllaLogoNuevo.png" alt="Logo" style="width: 10rem; height: auto; border-radius: 10%; margin-right: 1rem" />
+      </span>
+    </template>
 
-      <pv-card class="justify-content-center mt-2">
-        <template #content>
-          <i class="pi pi-clipboard icon-large"></i>
-        </template>
-        <template #footer>
-          <router-link :to="items[1].to"  rel="noopener">
-            <pv-button class="button-green" :label="items[1].label" />
-          </router-link>
-        </template>
-      </pv-card>
+    <template #center >
+      <div class="toolbar-section center-start ">
+        <router-link to="/control-panel">
+          <pv-button class="bg-transparent mr-4">
+            <p>{{ $t('toolbarControlPanel') }}</p>
+          </pv-button>
+        </router-link>
+        <router-link to="/crop-list-and-registration">
+          <pv-button class="bg-transparent mr-4">
+            <p>{{ $t('toolbarCropsRegistration')}}</p>
+          </pv-button>
+        </router-link>
+      </div>
 
-      <pv-card class="justify-content-center mt-2">
-        <template #content>
-          <i class="pi pi-comments icon-large"></i>
-        </template>
-        <template #footer>
-          <router-link :to="items[2].to"  rel="noopener">
-            <pv-button class="button-green" :label="items[2].label" />
-          </router-link>
-        </template>
-      </pv-card>
+    </template>
 
-      <pv-card class="justify-content-center mt-2">
-        <template #content>
-          <i class="pi pi-history icon-large"></i>
-        </template>
-        <template #footer>
-          <router-link :to="items[3].to"  rel="noopener">
-            <pv-button class="button-green" :label="items[3].label" />
-          </router-link>
-        </template>
-      </pv-card>
-
-
-    </div>
-
-    <div class="right-container">
-      <pv-card class="card-crop-control">
-        <template #content>
-          <h2>{{$t('seasonCropRecommendation')}}</h2>
-          <h3>{{ recommendedCrop?.name }}</h3>
-          <img :src="recommendedCrop?.image" alt="Crop image" class="fixed-size-image">
-          <p>{{ recommendedCrop?.description }}</p>
-        </template>
-      </pv-card>
-    </div>
-  </div>
-
+    <template #end>
+      <language-switcher/>
+      <router-link to="/user-profile-edit">
+        <pv-button class="toolbar-section user" :Ripple="false">
+          <i class="pi pi-user " style="font-size: 2rem"></i>
+          <p class="ml-3" >Roberto Juarez</p>
+        </pv-button>
+      </router-link>
+    </template>
+  </pv-toolbar>
 </template>
 
 <style scoped>
-.main-container {
+.toolbar-section {
+  display: inline-block;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+
+.user {
   display: flex;
-  margin-top:8em;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 1rem;
+  padding: 0.5rem;
+  height: auto !important;
+  width: auto !important;
+  transform: none !important;
 }
 
-.left-container,
-.right-container {
-  flex: 1;
+.user:active, .user:focus {
+  transform: none !important;
+}
+
+
+.fixed-toolbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+}
+
+.pv-toolbar {
   display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
+  justify-content: space-between;
+  background-color: var(--primary-main-green)
 }
 
-.icon-large {
-  font-size: 8em;
-  color: black;
-  margin-bottom: 0.1em;
+.center-start {
+  display: flex;
+  justify-content: flex-start;
 }
-
-.button-green {
-  background-color: var(--primary-main-green);
-  color: white;
-}
-.fixed-size-image {
-  width: 300px;
-  height: 300px;
-  border-radius:25px;
-}
-.card-crop-control{
-  background-color: var(--primary-main-green);
-  color: #ffffff;
-  border-radius: 25px;
-}
-
 </style>
