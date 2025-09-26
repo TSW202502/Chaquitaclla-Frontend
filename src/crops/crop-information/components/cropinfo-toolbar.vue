9f36db6e31f20f27dc8120cda55604f1a0ce1cda
JulyP <u20211d760@upc.edu.pe>
2025-09-26 11:36:55 -0500

    feat: add crop information toolbar component with navigation buttons

---
+++ src/crops/crop-information/components/cropinfo-toolbar.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  name: 'CropinfoToolbar',
+  data() {
+    return {
+      title: "Crop Information",
+      buttons: ["General Information", "Crop Care", "Controls", "Diseases or Pests", "Products Used"],
+      routes: ["/general-information", "/crop-care", "/controls", "/diseases-or-pests", "/products-used"],
+      activeButton: 0
+    };
+  },
+  methods: {
+    selectButton(index) {
+      this.activeButton = index;
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="title">{{ title }}</h1>
+    <div class="buttons">
+      <router-link v-for="(button, index) in buttons" :key="index" :to="routes[index]">
+        <button
+            :key="index"
+            :class="{ active: activeButton === index }"
+            @click="selectButton(index)"
+        >
+          {{ button }}
+        </button>
+      </router-link>
+    </div>
+    <div class="line"></div>
+  </div>
+</template>
