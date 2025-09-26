d56d98603d0e008de3f730e9bc9c3db7bb4c1f7e
JulyP <u20211d760@upc.edu.pe>
2025-09-26 11:34:54 -0500

    feat: add crop care component with calendar and custom table

---
+++ src/crops/crop-information/components/crop-care.component.vue
@@ -0,0 +1,30 @@
+<script>
+
+import CustomTable from "./custom-table.component.vue";
+
+export default {
+  components: {CustomTable},
+  data() {
+    return {
+      date: null,
+      tableHeaders: ['Date', 'Suggestion'],
+      tableData: [
+        ['25/07/2024', 'Water the crop for 30 min'],
+        ['05/08/2024', 'Water the crop for 30 min'],
+        ['15/08/2024', 'Water the crop for 30 min'],
+        ['10/09/2024', 'Water the crop for 30 min'],
+        ['15/09/2024', 'Water the crop for 30 min']
+      ]
+    };
+  }
+};
+</script>
+
+<template>
+  <div>
+    <pv-calendar  class="calendar" v-model="date" inline showWeek />
+    <custom-table class="table" :headers="tableHeaders" :data="tableData" />
+  </div>
+</template>
+
+
