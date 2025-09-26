deb691bee2c8cbc7a9c4f8e720885161e404472d
JulyP <u20211d760@upc.edu.pe>
2025-09-26 11:35:43 -0500

    feat: add crop information component with toolbar and router view

---
+++ src/crops/crop-information/components/crop-information.component.vue
@@ -0,0 +1,17 @@
+<script>
+import CropinfoToolbar from "./cropinfo-toolbar.vue";
+
+export default {
+  name: 'CropInformation',
+  components: {CropinfoToolbar},
+}
+</script>
+
+<template>
+  <div class="container">
+    <cropinfo-toolbar class="toolbar"></cropinfo-toolbar>
+    <router-view/>
+  </div>
+
+</template>
+
