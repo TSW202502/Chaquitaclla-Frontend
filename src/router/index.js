863e20ff5b9201d74c063c5a7d9c00402cdfa0a2
JulyP <u20211d760@upc.edu.pe>
2025-10-08 18:45:51 -0500

    feat: add route for Crop Information component

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import CropListAndRegistrationManagementComponent from "../crops/pages/crop-list-and-registration-management.component.vue";
+import CropInformationComponent from "../crops/crop-information/pages/crop-information.component.vue";
 
 
 const routes = [
     { path: '/', redirect: '/home'},
     { path:'/crop-list-and-registration',component: CropListAndRegistrationManagementComponent},
+    { path: '/crop-information', component: CropInformationComponent},
 
 ];
 
